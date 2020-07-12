0ee3f73b209d4864852b55885bbe365535c25a20
Felix Loehring <loehfelix@gmail.com>
2020-07-12 13:18:28 +0200

    changes PORT declaration

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 
 morgan.token("content", req => !req.body ? "" : JSON.stringify(req.body))
 
