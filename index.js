d28931b2b535f121b36757eb4a3081ca02c968a2
Felix Loehring <loehfelix@gmail.com>
2020-07-12 13:02:13 +0200

    add backend step 9 (cors)

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
-const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
+
+const app = express()
+app.use(cors())
 app.use(express.json())
 
 const PORT = 3001
