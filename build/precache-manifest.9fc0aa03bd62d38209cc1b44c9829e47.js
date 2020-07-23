fa1e60dbb8f14bd08486f5959293bd423913f2a2
Felix Loehring <loehfelix@gmail.com>
2020-07-23 15:50:32 +0200

    add build dir for frontend

---
+++ build/precache-manifest.9fc0aa03bd62d38209cc1b44c9829e47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cceca42b4f6be1bdbb8f8677337899d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4fa0b84a7f1ccec8e3f0",
+    "url": "/static/css/main.74d6e617.chunk.css"
+  },
+  {
+    "revision": "c2db7b13b6c244651373",
+    "url": "/static/js/2.d5e710d5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d5e710d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fa0b84a7f1ccec8e3f0",
+    "url": "/static/js/main.89fa48d8.chunk.js"
+  },
+  {
+    "revision": "68d5127cb03992670844",
+    "url": "/static/js/runtime-main.684b035f.js"
+  }
+]);
